ff74df7231cc8c645a643a0b3587dcf7a6b81f5c
kimball739 <carter.lommel@icloud.com>
2023-04-04 22:22:31 -0500

    test

--- src/main.cpp
+++ src/main.cpp
@@ -11,9 +11,39 @@ void disabled() {}
 void competition_initialize() {}
 
 void autonomous() {
+	//tracking wheel odom
+	///*
+	std::shared_ptr<OdomChassisController> chassis =
+        ChassisControllerBuilder()
+            .withMotors({-1, 2, -3}, {4, -5, 6})
+            // blue cartridge, 36:84 external gearing, 4.125 in wheel diam, 10.25 in wheel track
+			.withDimensions({AbstractMotor::gearset::blue, (36.0 / 84.0)}, {{4.125_in, 10.25_in}, imev5GreenTPR})
+			.withSensors(RotationSensor{7}, RotationSensor{8, true})
+			.withOdometry({{1.5625_in, 7_in}, quadEncoderTPR})
+			.buildOdometry();
+	//*/
+	/*
+	//integrated encoder odom
+	std::shared_ptr<OdomChassisController> chassis =
+        ChassisControllerBuilder()
+            .withMotors({-1, 2, -3}, {4, -5, 6})
+			.withGains(
+				{0.001, 0, 0.0001}, // Distance controller gains
+				{0.001, 0, 0.0001}, // Turn controller gains
+				{0.001, 0, 0.0001}  // Angle controller gains (helps drive straight)
+			)
+            // blue cartridge, 36:84 external gearing, 4.125 in wheel diam, 10.25 in wheel track
+			.withDimensions({AbstractMotor::gearset::blue, (36.0 / 84.0)}, {{4.125_in, 10.25_in}, imev5GreenTPR})
+			.withOdometry(okapi::StateMode::CARTESIAN)
+			.buildOdometry();
+	*/
+	chassis->setState({0_in, 0_in, 0_deg});
 	switch(selector::auton){
 		case 1:
-		//red left
+			//red left
+			chassis->driveToPoint({1_ft, 1_ft});
+			chassis->driveToPoint({0_ft, 2_ft});
+			chassis->turnToAngle(0_deg);
 		break;
 		case 2:
 		//red right
@@ -43,15 +73,11 @@ void opcontrol() {
             .withMotors({-1, 2, -3}, {4, -5, 6})
             // blue cartridge, 36:84 external gearing, 4 in wheel diam, 11.5 in wheel track
 			.withDimensions({AbstractMotor::gearset::blue, (36.0 / 84.0)}, {{4.125_in, 10.25_in}, imev5GreenTPR})
-			//.withSensors(RotationSensor{7}, RotationSensor{8, true})
-			//.withOdometry()
-			//.buildOdometry()
             .build();
 
 	while (true) {
 		drive->getModel()->tank(controller.getAnalog(ControllerAnalog::leftY),
-                            	controller.getAnalog(ControllerAnalog::rightY));
-
+        	                  	controller.getAnalog(ControllerAnalog::rightY));
 		pros::delay(20);
 	}
 }
