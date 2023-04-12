67c7e963d7fddebd13ec189fe5e03b825a0c43d4
kimball739 <carter.lommel@icloud.com>
2023-04-11 19:02:06 -0500

    operator control finished

--- src/main.cpp
+++ src/main.cpp
@@ -12,7 +12,7 @@ void competition_initialize() {}
 
 void autonomous() {
 	//integrated encoder odom
-	///*
+	/*
 	std::shared_ptr<OdomChassisController> bot =
         ChassisControllerBuilder()
             .withMotors({-1, 2, -3}, {4, -5, 6})
@@ -20,8 +20,7 @@ void autonomous() {
 			.withDimensions({AbstractMotor::gearset::blue, (36.0 / 84.0)}, {{4.125_in, 10.25_in}, imev5GreenTPR})
 			.withOdometry(StateMode::CARTESIAN)
 			.buildOdometry();
-	//*/
-	/*
+	*/
 	//tracking wheel odom
 	std::shared_ptr<OdomChassisController> bot =
         ChassisControllerBuilder()
@@ -34,14 +33,20 @@ void autonomous() {
             // blue cartridge, 36:84 external gearing, 4.125 in wheel diam, 10.25 in wheel track
 			.withDimensions({AbstractMotor::gearset::blue, (36.0 / 84.0)}, {{4.125_in, 10.25_in}, imev5GreenTPR})
 			.withSensors(RotationSensor{7}, RotationSensor{8, true})
-			.withOdometry({{1.5625_in, 7_in}, quadEncoderTPR}, StateMode::CARTESIAN)
+			.withOdometry({{1.5625_in, 6.875_in}, quadEncoderTPR})
 			.buildOdometry();
-	*/
+
+	ADIButton limitSwitch('A');
+
+	Motor catapult(9, true, AbstractMotor::gearset::red, AbstractMotor::encoderUnits::degrees);
+	Motor intake(10, true, AbstractMotor::gearset::green, AbstractMotor::encoderUnits::degrees);
+
+
 	bot->setState({0_in, 0_in, 0_deg});
 	switch(selector::auton){
 		case 1: //red near
 			bot->driveToPoint({1_ft, 1_ft});
-			bot->driveToPoint({0_ft, 2_ft});
+			bot->driveToPoint({2_ft, 0_ft});
 			bot->driveToPoint({0_ft, 0_ft}, false);
 			break;
 		case 2: //red far
@@ -63,9 +68,21 @@ void autonomous() {
 }
 
 void opcontrol() {
+	bool cataReady=1;
+
 	Controller controller;
-	ControllerButton cataTrigger(ControllerDigital::L1);
-	Motor catapult(9);
+
+	ControllerButton shootButton(ControllerDigital::L1);
+	ControllerButton intakeCylinderButton(ControllerDigital::L2);
+	ControllerButton intakeButton(ControllerDigital::R1);
+	ControllerButton outakeButton(ControllerDigital::R2);
+
+	ADIButton limitSwitch('A');
+	pros::ADIDigitalOut intakeCylinder ('B', 0);
+
+	Motor catapult(9, true, AbstractMotor::gearset::red, AbstractMotor::encoderUnits::degrees);
+	Motor intake(10, true, AbstractMotor::gearset::green, AbstractMotor::encoderUnits::degrees);
+
 	std::shared_ptr<ChassisController> drive =
         ChassisControllerBuilder()
             .withMotors({-1, 2, -3}, {4, -5, 6})
@@ -74,13 +91,31 @@ void opcontrol() {
             .build();
 
 	while (true) {
-		drive->getModel()->tank(controller.getAnalog(ControllerAnalog::leftY),
-        	                  	controller.getAnalog(ControllerAnalog::rightY));
-		if(cataTrigger.isPressed()){
-			catapult.moveVoltage(12000);
+		drive->getModel()->tank(controller.getAnalog(ControllerAnalog::leftY), 
+								controller.getAnalog(ControllerAnalog::rightY));
+
+		if(!limitSwitch.isPressed()){
+			catapult.moveVoltage(11000);
+		}else if(shootButton.isPressed()){
+			catapult.moveVoltage(11000);
 		}else{
 			catapult.moveVoltage(0);
 		}
+
+		if(outakeButton.isPressed()){
+			intake.moveVoltage(10000);
+		}else if(intakeButton.isPressed() && limitSwitch.isPressed()){
+			intake.moveVoltage(-10000);
+		}else{
+			intake.moveVoltage(0);
+		}
+
+		if(intakeCylinderButton.isPressed()){
+			intakeCylinder.set_value(1);
+		}else{
+			intakeCylinder.set_value(0);
+		}
+
 		pros::delay(20);
 	}
 }
